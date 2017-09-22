5b03bb82dddf5a7f4237114a67e1369b6fd274ac
nazio11 <31521430+nazio11@users.noreply.github.com>
2017-09-22 11:00:43 -0300

    Update finalizar.jsx
    Adicionando funções de Finalizar e Cancelar com alert, apenas para teste.

--- frontend/src/vendas/finalizar.jsx
+++ frontend/src/vendas/finalizar.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
+export default class Finalizar extends Component {
+	constructor(props) {
+		super(props) 
+			this.state = {
 
-	<div className = 'btn-group-small'>
-		<button type = 'button' className = 'btn btn-primary' > 
-			Finalizar 
-		</button>
-				
-		<button type = 'button' className = 'btn btn-danger' >
-			Cancelar 
-		</button>
-	</div>
-)
+			}
+		this.alertaFinaliza = this.alertaFinaliza.bind(this);
+		this.alertaCancela = this.alertaCancela.bind(this);
+	}
+
+	alertaFinaliza(){
+		alert("Por favor, realize o pagamento para fazer o Download dos ingressos!");
+	}
+
+	alertaCancela(){
+		alert("Compra Cancelada com Sucesso!! :(");
+	}
+
+	render() {
+
+		return (
+
+			<div className = 'btn-group-small'>
+			
+				<button type = 'button' className = 'btn btn-primary' 
+					onClick = {this.alertaFinaliza}> Finalizar </button>
+
+				<button type = 'button' className = 'btn btn-danger'
+					onClick = {this.alertaCancela}> Cancelar </button>
+
+			</div>
+		);
+	}
+}
+
